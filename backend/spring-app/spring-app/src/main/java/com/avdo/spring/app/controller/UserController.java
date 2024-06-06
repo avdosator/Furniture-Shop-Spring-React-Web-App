5acd459f4d82847edcfe5b948dd104cf0c7e24c5
Avdo Sator <avdo.sator@hotmail.com>
2024-06-06 12:42:06 +0200

    Add dependencies to controller and service for User

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/controller/UserController.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.avdo.spring.app.controller;
 
+import com.avdo.spring.app.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    @Autowired
+    private UserService userService;
 }
--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/service/UserService.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/service/UserService.java
@@ -1,4 +1,12 @@
 package com.avdo.spring.app.service;
 
+import com.avdo.spring.app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
 }
