ba2f6cb1a61e107a3088195666ea35d09fb21228
Avdo Sator <avdo.sator@hotmail.com>
2024-06-06 16:07:19 +0200

    Add changes to UserController

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/controller/UserController.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.avdo.spring.app.controller;
 
+import com.avdo.spring.app.dto.CreateUserRequest;
 import com.avdo.spring.app.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +15,10 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @PostMapping("/users")
+    ResponseEntity<?> createUser (@Valid @RequestBody CreateUserRequest createUserRequest) {
+        return  ResponseEntity.status(HttpStatus.CREATED).body("User successfully created");
+    }
+
 }
