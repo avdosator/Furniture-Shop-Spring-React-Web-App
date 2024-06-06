a35ed6753e3d372b73a8e4bb1967b00aa6a22989
Avdo Sator <avdo.sator@hotmail.com>
2024-06-06 21:08:36 +0200

    Use bcrypt to hash password before saving it to database

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/service/UserServiceImpl.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/service/UserServiceImpl.java
@@ -4,27 +4,34 @@ import com.avdo.spring.app.dto.CreateUserRequest;
 import com.avdo.spring.app.entity.User;
 import com.avdo.spring.app.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements  UserService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @Autowired
-    UserRepository userRepository;
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     public void createUser(CreateUserRequest createUserRequest) {
-        User user = UserMapper.mapToUser(createUserRequest);
+        User user = new UserMapper().mapToUser(createUserRequest);
         userRepository.save(user);
     }
 
-    public static class UserMapper {
-        public static User mapToUser(CreateUserRequest createUserRequest) {
+    public  class UserMapper {
+        public User mapToUser(CreateUserRequest createUserRequest) {
             User user = new User();
             user.setUsername(createUserRequest.getUsername());
             user.setFirstname(createUserRequest.getFirstname());
             user.setLastname(createUserRequest.getLastname());
-            user.setPassword(createUserRequest.getPassword());
+            user.setPassword(passwordEncoder.encode(createUserRequest.getPassword()));
             user.setEmail(createUserRequest.getEmail());
             return user;
         }
