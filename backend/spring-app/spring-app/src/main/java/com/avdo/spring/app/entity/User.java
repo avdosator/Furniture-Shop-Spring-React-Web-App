2956a18b7973d744cfe8324ebb000fb0ad6e36ac
Avdo Sator <avdo.sator@hotmail.com>
2024-06-11 14:28:50 +0200

    Add field role of type String to User class

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/entity/User.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/entity/User.java
@@ -37,6 +37,8 @@ public class User implements UserDetails {
     @Column(name = "date_created")
     private Date dateCreated;
 
+    @Column(name = "role")
+    private String role;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
