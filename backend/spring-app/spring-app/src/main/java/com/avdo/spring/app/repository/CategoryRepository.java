78e32feb1e4b9645ed62a72277710a878426d17e
Avdo Sator <avdo.sator@hotmail.com>
2024-06-13 12:40:25 +0200

    Create CategoryRepository interface

---
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/repository/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.avdo.spring.app.repository;
+
+import com.avdo.spring.app.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findByCategoryName(String name);
+}
