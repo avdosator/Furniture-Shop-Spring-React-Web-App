f3f71e8fceac4a82806cad236cc4989b950752b7
Avdo Sator <avdo.sator@hotmail.com>
2024-06-13 15:33:24 +0200

    Implement routes and logic for reading categories

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/controller/CategoryController.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/controller/CategoryController.java
@@ -9,11 +9,9 @@ import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController()
@@ -43,5 +41,20 @@ public class CategoryController {
             String successMessage = "Successfully created category " + createCategoryRequest.getName();
             return ResponseEntity.status(HttpStatus.CREATED).body(successMessage);
         }
+    }
+
+    @GetMapping("/categories")
+    public ResponseEntity<List<Category>> findAllCategories() {
+        List<Category> categories = categoryService.findAllCategories();
+        return ResponseEntity.ok(categories);
+    }
+
+    @GetMapping("/categories/{name}")
+    public ResponseEntity<Category> findCategoryByName(@PathVariable String name) {
+        Category category = categoryService.findCategoryByName(name);
+        if (category != null) {
+            return ResponseEntity.ok(category);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
