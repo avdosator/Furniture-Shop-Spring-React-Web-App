93b24ca20cdf71dc299bbbbae2ad2fba3dc03871
Avdo Sator <avdo.sator@hotmail.com>
2024-06-13 13:01:41 +0200

    Create CategoryService with create and read functionalities

---
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/service/CategoryService.java
@@ -0,0 +1,35 @@
+package com.avdo.spring.app.service;
+
+import com.avdo.spring.app.entity.Category;
+import com.avdo.spring.app.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public void createCategory(CreateCategoryRequest createCategoryRequest) {
+        Category category = new Category();
+        categoryRepository.save(category);
+    }
+
+    public List<Category> findAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Category findCategoryByName(String name) {
+        Category category = categoryRepository.findCategoryByName(name)
+                .orElseThrow();
+    }
+}
