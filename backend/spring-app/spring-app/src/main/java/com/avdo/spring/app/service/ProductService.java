2eb87cff20be7036af10db87bdc72f1685ac62fc
Avdo Sator <avdo.sator@hotmail.com>
2024-06-14 10:40:49 +0200

    Create ProductService class

---
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.avdo.spring.app.service;
+
+import com.avdo.spring.app.repository.CategoryRepository;
+import com.avdo.spring.app.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository, CategoryRepository categoryRepository) {
+        this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
+    }
+}
