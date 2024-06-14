a17efa937a6b455bb8151f03ea07fe9fcd0db89e
Avdo Sator <avdo.sator@hotmail.com>
2024-06-14 15:29:50 +0200

    Create PrOductController and implement route for product creation

---
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/controller/ProductController.java
@@ -0,0 +1,41 @@
+package com.avdo.spring.app.controller;
+
+import com.avdo.spring.app.dto.CreateProductRequest;
+import com.avdo.spring.app.service.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/database")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping("/products")
+    public ResponseEntity<String> createProduct(@Valid @RequestBody CreateProductRequest createProductRequest,
+                                                BindingResult result) {
+        if (result.hasErrors()) {
+            List<String> errors = result.getAllErrors()
+                    .stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .toList();
+            return ResponseEntity.badRequest().body(errors.toString());
+        } else {
+            productService.createProduct(createProductRequest);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Successfully created product!");
+        }
+    }
+}
