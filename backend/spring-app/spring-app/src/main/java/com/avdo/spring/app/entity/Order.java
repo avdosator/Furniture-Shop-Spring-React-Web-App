97a1c33822719c0379a4a4ba521bcd17f0ca4434
Avdo Sator <avdo.sator@hotmail.com>
2024-06-15 10:36:33 +0200

    Create Order entity

---
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/entity/Order.java
@@ -0,0 +1,31 @@
+package com.avdo.spring.app.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.sql.Date;
+
+@Entity
+@Table(name = "order")
+@Data
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
+
+
+    private User user;
+
+    @Column(name = "total_amount")
+    private double totalAmount;
+
+    @Column(name = "order_status")
+    private String orderStatus;
+
+    @Column(name = "date_created")
+    private Date dateCreated;
+
+
+}
