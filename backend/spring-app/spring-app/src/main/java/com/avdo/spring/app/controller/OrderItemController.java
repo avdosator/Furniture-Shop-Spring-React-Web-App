5f7ff977bbf3e6a5b0229266323a390867b13589
Avdo Sator <avdo.sator@hotmail.com>
2024-06-17 16:35:02 +0200

    Create OrderItem controller class

---
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/controller/OrderItemController.java
@@ -0,0 +1,18 @@
+package com.avdo.spring.app.controller;
+
+import com.avdo.spring.app.service.OrderItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/database")
+public class OrderItemController {
+
+    private final OrderItemService orderItemService;
+
+    @Autowired
+    public OrderItemController(OrderItemService orderItemService) {
+        this.orderItemService = orderItemService;
+    }
+}
