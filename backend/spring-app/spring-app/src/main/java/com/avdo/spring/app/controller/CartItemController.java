bd840ad189190b539d88b571734cbbb995c411a0
Avdo Sator <avdo.sator@hotmail.com>
2024-06-18 11:56:00 +0200

    Create CartItem controller class

---
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/controller/CartItemController.java
@@ -0,0 +1,18 @@
+package com.avdo.spring.app.controller;
+
+import com.avdo.spring.app.service.CartItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/database")
+public class CartItemController {
+
+    private final CartItemService cartItemService;
+
+    @Autowired
+    public CartItemController(CartItemService cartItemService) {
+        this.cartItemService = cartItemService;
+    }
+}
