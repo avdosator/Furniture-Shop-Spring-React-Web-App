bf1d610ee3c7367f4021cf221d9d3afd0ec60ef4
Avdo Sator <avdo.sator@hotmail.com>
2024-06-18 10:43:05 +0200

    Add fields to CartItem class

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/entity/CartItem.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/entity/CartItem.java
@@ -1,11 +1,25 @@
 package com.avdo.spring.app.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.Date;
 
 @Entity
 @Table(name = "cart_item")
 @Data
 public class CartItem {
+
+    @Id
+    @Column(name = "cart_item_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Cart cart;
+
+    private Product product;
+
+    private int quantity;
+
+    private Date dateCreated;
 }
