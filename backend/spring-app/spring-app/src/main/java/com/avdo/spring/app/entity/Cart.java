29749fda6dc5474f639953b378c793a0f60dfb88
Avdo Sator <avdo.sator@hotmail.com>
2024-06-18 10:39:22 +0200

    Add fields with needed annotations to Cart class

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/entity/Cart.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/entity/Cart.java
@@ -3,6 +3,8 @@ package com.avdo.spring.app.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.sql.Date;
+
 @Entity
 @Table(name = "cart")
 @Data
@@ -12,4 +14,11 @@ public class Cart {
     @Column(name = "cart_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private User user;
+
+    @Column(name = "date_created", updatable = false)
+    private Date dateCreated;
 }
