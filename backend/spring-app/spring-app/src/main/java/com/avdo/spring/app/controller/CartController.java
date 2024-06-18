7d958108c84a479f2607a3a87b8c5370c3cb77c6
Avdo Sator <avdo.sator@hotmail.com>
2024-06-18 12:39:26 +0200

    Add endpoint for creating a cart

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/controller/CartController.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/controller/CartController.java
@@ -2,6 +2,8 @@ package com.avdo.spring.app.controller;
 
 import com.avdo.spring.app.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +16,12 @@ public class CartController {
     @Autowired
     public CartController(CartService cartService) {
         this.cartService = cartService;
+    }
+
+    // create cart for user with password userpassword
+    @PostMapping("/create-cart")
+    public ResponseEntity<String> createCart() {
+        cartService.createCart();
+        return ResponseEntity.ok("Successfully created cart for user \"user\"");
     }
 }
