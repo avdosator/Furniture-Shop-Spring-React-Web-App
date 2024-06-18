6afe499191a93d506781d9657e1d6124ad4c618b
Avdo Sator <avdo.sator@hotmail.com>
2024-06-18 11:50:11 +0200

    Create CartItem service class

---
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/service/CartItemService.java
@@ -0,0 +1,16 @@
+package com.avdo.spring.app.service;
+
+import com.avdo.spring.app.repository.CartItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartItemService {
+
+    private final CartItemRepository cartItemRepository;
+
+    @Autowired
+    public CartItemService(CartItemRepository cartItemRepository) {
+        this.cartItemRepository = cartItemRepository;
+    }
+}
