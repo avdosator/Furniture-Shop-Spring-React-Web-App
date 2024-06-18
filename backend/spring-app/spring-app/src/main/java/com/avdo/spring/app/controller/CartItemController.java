2f9b4289465604997e2bc9b22fbaee52f9215822
Avdo Sator <avdo.sator@hotmail.com>
2024-06-18 14:45:19 +0200

    Add endpoint for creating cart item

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/controller/CartItemController.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/controller/CartItemController.java
@@ -2,6 +2,9 @@ package com.avdo.spring.app.controller;
 
 import com.avdo.spring.app.service.CartItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +17,12 @@ public class CartItemController {
     @Autowired
     public CartItemController(CartItemService cartItemService) {
         this.cartItemService = cartItemService;
+    }
+
+    // create cart item with some product from database (testing)
+    @PostMapping("/create-cart-item")
+    public ResponseEntity<String> createCartItem() {
+        cartItemService.createCartItem();
+        return ResponseEntity.ok("Successfully created cart item");
     }
 }
