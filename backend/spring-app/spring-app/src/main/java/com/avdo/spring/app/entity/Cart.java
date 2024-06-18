adb489a370afad66ebcda60b68a63ce15eb5109a
Avdo Sator <avdo.sator@hotmail.com>
2024-06-18 11:34:01 +0200

    Add needed annotations to Cart and CartItem classes and its fields

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/entity/Cart.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/entity/Cart.java
@@ -1,13 +1,17 @@
 package com.avdo.spring.app.entity;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import lombok.Data;
 
 import java.sql.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "cart")
 @Data
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Cart {
 
     @Id
@@ -18,6 +22,9 @@ public class Cart {
     @ManyToOne
     @JoinColumn(name = "id")
     private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> items;
 
     @Column(name = "date_created", updatable = false)
     private Date dateCreated;
--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/entity/CartItem.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/entity/CartItem.java
@@ -1,5 +1,7 @@
 package com.avdo.spring.app.entity;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -8,6 +10,7 @@ import java.util.Date;
 @Entity
 @Table(name = "cart_item")
 @Data
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class CartItem {
 
     @Id
@@ -15,11 +18,18 @@ public class CartItem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
     private Cart cart;
 
+    // we don't need bidirectional relationship with Product class? Don't need field of type CartItem there?
+    @ManyToOne
+    @JoinColumn(name = "product_id")
     private Product product;
 
+    @Column(name = "quantity")
     private int quantity;
 
+    @Column(name = "date_created")
     private Date dateCreated;
 }
