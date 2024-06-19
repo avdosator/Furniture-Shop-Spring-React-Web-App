4969af87d224bf22c2912c9f91bf13e1bfa3a352
Avdo Sator <avdo.sator@hotmail.com>
2024-06-19 21:25:29 +0200

    Add method for order creation in OrderController

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/controller/OrderController.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/controller/OrderController.java
@@ -1,7 +1,13 @@
 package com.avdo.spring.app.controller;
 
+import com.avdo.spring.app.dto.CreateOrderRequest;
+import com.avdo.spring.app.entity.Order;
 import com.avdo.spring.app.service.OrderService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +20,11 @@ public class OrderController {
     @Autowired
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
+    }
+
+    @PostMapping("/order")
+    public ResponseEntity<Order> createOrder(@Valid @RequestBody CreateOrderRequest createOrderRequest) {
+        Order order = orderService.createOrder(createOrderRequest);
+        return ResponseEntity.ok(order);
     }
 }
