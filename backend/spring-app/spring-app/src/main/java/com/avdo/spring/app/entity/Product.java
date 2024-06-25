7c949c1e5f5531656068dc42644ecde72c6b520d
Avdo Sator <avdo.sator@hotmail.com>
2024-06-25 14:39:24 +0200

    Add attribute to orderItems field's annotation

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/entity/Product.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/entity/Product.java
@@ -34,7 +34,7 @@ public class Product {
     @JoinColumn(name = "category_id")
     private Category category;
 
-    @OneToMany(mappedBy = "product")
+    @OneToMany(mappedBy = "product", fetch = FetchType.EAGER)
     private List<OrderItem> orderItems;
 
 }
