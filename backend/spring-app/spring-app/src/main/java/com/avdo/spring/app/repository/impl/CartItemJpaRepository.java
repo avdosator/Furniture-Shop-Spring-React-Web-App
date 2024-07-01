2b5e41247e92ff6136847c5f0a6989ede8309d88
Avdo Sator <avdo.sator@hotmail.com>
2024-07-01 18:14:26 +0200

    Put @Repository annotation on CartItemJpaRepository class

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/repository/impl/CartItemJpaRepository.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/repository/impl/CartItemJpaRepository.java
@@ -3,12 +3,13 @@ package com.avdo.spring.app.repository.impl;
 import com.avdo.spring.app.entity.CartItemEntity;
 import com.avdo.spring.app.repository.CrudCartItemRepository;
 import com.avdo.spring.app.service.domain.model.CartItem;
-import jdk.dynalink.linker.LinkerServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class CartItemJpaRepository implements CartItemRepository {
 
     @Autowired
