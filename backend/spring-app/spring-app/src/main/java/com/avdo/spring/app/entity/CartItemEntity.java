2e5d4d2ae5713945f9d3c7f1ca9e8ca6ff13102d
Avdo Sator <avdo.sator@hotmail.com>
2024-07-08 22:37:52 +0200

    Clear CartItemEntity of unused methods and loggings

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/entity/CartItemEntity.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/entity/CartItemEntity.java
@@ -37,7 +37,7 @@ public class CartItemEntity {
     public CartItem toDomainModel() {
         return CartItem.builder()
                 .id(this.id)
-                .cart(this.cartEntity.toDomainModel())
+                .cartId(this.cartEntity.getId())
                 .product(this.product)
                 .quantity(this.quantity)
                 .dateCreated(this.dateCreated)
@@ -82,48 +82,6 @@ public class CartItemEntity {
 
     public void setDateCreated(Date dateCreated) {
         this.dateCreated = dateCreated;
-    }
-
-    public boolean equals(final Object o) {
-        if (o == this) return true;
-        if (!(o instanceof CartItemEntity)) return false;
-        final CartItemEntity other = (CartItemEntity) o;
-        if (!other.canEqual((Object) this)) return false;
-        final Object this$id = this.getId();
-        final Object other$id = other.getId();
-        if (this$id == null ? other$id != null : !this$id.equals(other$id)) return false;
-        final Object this$cartEntity = this.getCartEntity();
-        final Object other$cartEntity = other.getCartEntity();
-        if (this$cartEntity == null ? other$cartEntity != null : !this$cartEntity.equals(other$cartEntity))
-            return false;
-        final Object this$product = this.getProduct();
-        final Object other$product = other.getProduct();
-        if (this$product == null ? other$product != null : !this$product.equals(other$product)) return false;
-        if (this.getQuantity() != other.getQuantity()) return false;
-        final Object this$dateCreated = this.getDateCreated();
-        final Object other$dateCreated = other.getDateCreated();
-        if (this$dateCreated == null ? other$dateCreated != null : !this$dateCreated.equals(other$dateCreated))
-            return false;
-        return true;
-    }
-
-    protected boolean canEqual(final Object other) {
-        return other instanceof CartItemEntity;
-    }
-
-    public int hashCode() {
-        final int PRIME = 59;
-        int result = 1;
-        final Object $id = this.getId();
-        result = result * PRIME + ($id == null ? 43 : $id.hashCode());
-        final Object $cartEntity = this.getCartEntity();
-        result = result * PRIME + ($cartEntity == null ? 43 : $cartEntity.hashCode());
-        final Object $product = this.getProduct();
-        result = result * PRIME + ($product == null ? 43 : $product.hashCode());
-        result = result * PRIME + this.getQuantity();
-        final Object $dateCreated = this.getDateCreated();
-        result = result * PRIME + ($dateCreated == null ? 43 : $dateCreated.hashCode());
-        return result;
     }
 
     public String toString() {
