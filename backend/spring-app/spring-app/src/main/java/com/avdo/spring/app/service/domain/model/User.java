7c0b266b2aa1659c160d7f81603f842c1409aa27
Avdo Sator <avdo.sator@hotmail.com>
2024-07-09 15:24:23 +0200

    Remove setters from User and make fields final

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/service/domain/model/User.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/service/domain/model/User.java
@@ -3,33 +3,31 @@ package com.avdo.spring.app.service.domain.model;
 import com.avdo.spring.app.entity.Order;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.sql.Date;
 import java.util.List;
 
-@Setter
 @Getter
 @Builder
 public class User {
 
-    private Long id;
+    private final Long id;
 
-    private String firstname;
+    private final String firstname;
 
-    private String lastname;
+    private final String lastname;
 
-    private String username;
+    private final String username;
 
-    private String email;
+    private final String email;
 
-    private String password;
+    private final String password;
 
-    private Date dateCreated;
+    private final Date dateCreated;
 
-    private String role;
+    private final String role;
 
-    private List<Order> orders;
+    private final List<Order> orders;
 
 
     public String toString() {
