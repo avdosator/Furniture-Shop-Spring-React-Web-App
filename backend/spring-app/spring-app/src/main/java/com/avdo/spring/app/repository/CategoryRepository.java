f0aa15560ec3478cd32db76e0f9c797543ec9fc2
Avdo Sator <avdo.sator@hotmail.com>
2024-07-10 15:03:59 +0200

    Move methods from CategoryServiceImpl to CategoryJpaRepository

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/repository/CategoryRepository.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/repository/CategoryRepository.java
@@ -1,12 +1,17 @@
 package com.avdo.spring.app.repository;
 
 import com.avdo.spring.app.entity.CategoryEntity;
+import com.avdo.spring.app.service.domain.model.Category;
+import com.avdo.spring.app.service.domain.request.CreateCategoryRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
+public interface CategoryRepository {
 
-    Optional<CategoryEntity> findCategoryEntityByName(String name);
+    Category createCategory(CreateCategoryRequest createCategoryRequest);
+    List<Category> findAllCategories();
+    Category findCategoryByName(String name);
 }
--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/repository/impl/CategoryJpaRepository.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/repository/impl/CategoryJpaRepository.java
@@ -1,16 +1,40 @@
 package com.avdo.spring.app.repository.impl;
 
+import com.avdo.spring.app.entity.CategoryEntity;
+import com.avdo.spring.app.repository.CategoryRepository;
 import com.avdo.spring.app.repository.crud.CrudCategoryRepository;
+import com.avdo.spring.app.service.domain.model.Category;
+import com.avdo.spring.app.service.domain.request.CreateCategoryRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public class CategoryJpaRepository {
+public class CategoryJpaRepository implements CategoryRepository {
 
     private final CrudCategoryRepository crudCategoryRepository;
 
     @Autowired
     public CategoryJpaRepository(CrudCategoryRepository crudCategoryRepository) {
         this.crudCategoryRepository = crudCategoryRepository;
+    }
+
+    @Override
+    public Category createCategory(CreateCategoryRequest createCategoryRequest) {
+        CategoryEntity category = new CategoryEntity();
+        category.setName(createCategoryRequest.getName());
+        return crudCategoryRepository.save(category);
+    }
+
+    @Override
+    public List<Category> findAllCategories() {
+        return crudCategoryRepository.findAll();
+    }
+
+    @Override
+    public Category findCategoryByName(String name) {
+        return crudCategoryRepository.findCategoryEntityByName(name)
+                .orElseThrow();
     }
 }
