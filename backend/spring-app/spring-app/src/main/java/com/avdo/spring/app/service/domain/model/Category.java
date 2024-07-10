ae0369788f290a29efaff41f46b4ad58a8ce924e
Avdo Sator <avdo.sator@hotmail.com>
2024-07-10 14:44:03 +0200

    Add fields to Category and implement toString method

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/service/domain/model/Category.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/service/domain/model/Category.java
@@ -1,4 +1,23 @@
 package com.avdo.spring.app.service.domain.model;
 
+import com.avdo.spring.app.entity.Product;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
 public class Category {
+
+    private Long id;
+
+    private String name;
+
+    private List<Product> products;
+
+    public String toString() {
+        return "Category(id=" + this.getId() +
+                ", name=" + this.getName() +  ")";
+    }
 }
