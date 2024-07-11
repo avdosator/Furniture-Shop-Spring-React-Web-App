072e4aaa3a95f96b67b51c28c9d2b01d7e801262
Avdo Sator <avdo.sator@hotmail.com>
2024-07-11 17:08:06 +0200

    Add fields to Product domain model

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/service/domain/model/Product.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/service/domain/model/Product.java
@@ -1,9 +1,37 @@
 package com.avdo.spring.app.service.domain.model;
 
+import com.avdo.spring.app.entity.CategoryEntity;
+import com.avdo.spring.app.entity.OrderItem;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 @Builder
 public class Product {
+
+    private Long id;
+
+    private String name;
+
+    private double price;
+
+    private int stock;
+
+    private String description;
+
+    private CategoryEntity categoryEntity;
+
+    private List<OrderItem> orderItems;
+
+    public String toString() {
+        return "Product(id=" + this.getId() +
+                ", name=" + this.getName() +
+                ", price=" + this.getPrice() +
+                ", stock=" + this.getStock() +
+                ", description=" + this.getDescription() +
+                ", category=" + this.categoryEntity.getName() + ")";
+    }
 }
