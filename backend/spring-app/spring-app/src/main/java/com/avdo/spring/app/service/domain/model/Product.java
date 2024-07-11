9b414ea91a69567b652c5f6df44b244220129cb3
Avdo Sator <avdo.sator@hotmail.com>
2024-07-11 17:36:39 +0200

    Make fields final in Product model

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/service/domain/model/Product.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/service/domain/model/Product.java
@@ -1,8 +1,7 @@
 package com.avdo.spring.app.service.domain.model;
 
-import com.avdo.spring.app.entity.CategoryEntity;
 import com.avdo.spring.app.entity.OrderItem;
-import jakarta.persistence.*;
+import com.avdo.spring.app.service.domain.model.Category;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,19 +11,19 @@ import java.util.List;
 @Builder
 public class Product {
 
-    private Long id;
+    private final Long id;
 
-    private String name;
+    private final String name;
 
-    private double price;
+    private final double price;
 
-    private int stock;
+    private final int stock;
 
-    private String description;
+    private final String description;
 
-    private CategoryEntity categoryEntity;
+    private final Category category;
 
-    private List<OrderItem> orderItems;
+    private final List<OrderItem> orderItems;
 
     public String toString() {
         return "Product(id=" + this.getId() +
@@ -32,6 +31,6 @@ public class Product {
                 ", price=" + this.getPrice() +
                 ", stock=" + this.getStock() +
                 ", description=" + this.getDescription() +
-                ", category=" + this.categoryEntity.getName() + ")";
+                ", category=" + this.category.getName() + ")";
     }
 }
