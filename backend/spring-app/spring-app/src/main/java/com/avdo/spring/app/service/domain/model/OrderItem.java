985fbcdc4c9ff92aed1ab30ef611f28b314532c2
Avdo Sator <avdo.sator@hotmail.com>
2024-07-19 20:44:17 +0200

    Add fields and implement toString method of OrderItem

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/service/domain/model/OrderItem.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/service/domain/model/OrderItem.java
@@ -1,9 +1,24 @@
 package com.avdo.spring.app.service.domain.model;
 
+import com.avdo.spring.app.entity.Order;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
 public class OrderItem {
+
+    private final Long id;
+    private final Order order;
+    private final int quantity;
+    private final double price;
+    private final Product product;
+
+    public String toString() {
+        return "OrderItem(id=" + this.getId() +
+                ", orderId=" + this.getOrder().getId() +
+                ", quantity=" + this.getQuantity() +
+                ", price=" + this.getPrice() +
+                ", productId=" + this.getProduct().getId() + ")";
+    }
 }
