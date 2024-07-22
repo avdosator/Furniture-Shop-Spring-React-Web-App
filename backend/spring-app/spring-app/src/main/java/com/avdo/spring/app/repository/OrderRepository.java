ee5c209793f7d4960ef460a495662d9e4d1ae3a3
Avdo Sator <avdo.sator@hotmail.com>
2024-07-22 22:55:04 +0200

    Create OrderJpaRepository class

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/repository/OrderRepository.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/repository/OrderRepository.java
@@ -1,9 +1,7 @@
 package com.avdo.spring.app.repository;
 
-import com.avdo.spring.app.entity.OrderEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+public interface OrderRepository {
 }
