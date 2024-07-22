045a73af6677f92ac7ff74584c65552a2cfbf151
Avdo Sator <avdo.sator@hotmail.com>
2024-07-22 23:11:59 +0200

    Implement logic in method that returns all orders

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/repository/impl/OrderJpaRepository.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/repository/impl/OrderJpaRepository.java
@@ -1,10 +1,14 @@
 package com.avdo.spring.app.repository.impl;
 
+import com.avdo.spring.app.entity.OrderEntity;
 import com.avdo.spring.app.repository.OrderRepository;
 import com.avdo.spring.app.repository.crud.CrudOrderRepository;
 import com.avdo.spring.app.service.domain.model.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class OrderJpaRepository implements OrderRepository {
@@ -20,5 +24,15 @@ public class OrderJpaRepository implements OrderRepository {
     public Order findById(Long id) {
         Order order = crudOrderRepository.findById(id).orElseThrow(() -> new RuntimeException("There is no order with that ID"));
         return order.toDomainModel();
+    }
+
+    @Override
+    public List<Order> findAllOrders() {
+        List<OrderEntity> orderEntities = crudOrderRepository.findAll();
+        List<Order> orders = new ArrayList<>();
+        for (OrderEntity orderEntity : orderEntities) {
+            orders.add(orderEntity.toDomainModel());
+        }
+        return orders;
     }
 }
--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/service/impl/OrderServiceImpl.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/service/impl/OrderServiceImpl.java
@@ -42,7 +42,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public List<Order> findAllOrders() {
-        return orderRepository.findAll();
+        return orderRepository.findAllOrders();
     }
 
     @Transactional
