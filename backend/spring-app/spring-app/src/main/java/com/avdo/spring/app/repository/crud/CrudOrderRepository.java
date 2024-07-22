11957077a94ef7ac199b69df232628788ebea498
Avdo Sator <avdo.sator@hotmail.com>
2024-07-22 22:53:32 +0200

    Create CrudOrderRepository interface

---
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/repository/crud/CrudOrderRepository.java
@@ -0,0 +1,7 @@
+package com.avdo.spring.app.repository.crud;
+
+import com.avdo.spring.app.entity.OrderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CrudOrderRepository extends JpaRepository<OrderEntity, Long> {
+}
