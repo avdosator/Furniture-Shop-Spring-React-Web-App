4401ba0f7e487de6c71562958fb9ba9ea14a6271
Avdo Sator <avdo.sator@hotmail.com>
2024-07-22 22:39:19 +0200

    Split OrderService into interface and implementation class

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/controller/OrderController.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/controller/OrderController.java
@@ -1,12 +1,10 @@
 package com.avdo.spring.app.controller;
 
 import com.avdo.spring.app.controller.dto.CreateOrderDto;
-import com.avdo.spring.app.entity.OrderEntity;
 import com.avdo.spring.app.service.OrderService;
 import com.avdo.spring.app.service.domain.model.Order;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
