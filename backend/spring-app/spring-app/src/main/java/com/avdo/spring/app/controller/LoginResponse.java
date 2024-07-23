447775e1e333318039eeaa3beee6670824da63f5
Avdo Sator <avdo.sator@hotmail.com>
2024-07-23 21:59:06 +0200

    Delombok annotations in LoginResponse class

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/controller/LoginResponse.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/controller/LoginResponse.java
@@ -1,14 +1,23 @@
 package com.avdo.spring.app.controller;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class LoginResponse {
 
     private String token;
-
     private long expiresIn;
 
+    public String getToken() {
+        return this.token;
+    }
+
+    public long getExpiresIn() {
+        return this.expiresIn;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public void setExpiresIn(long expiresIn) {
+        this.expiresIn = expiresIn;
+    }
 }
