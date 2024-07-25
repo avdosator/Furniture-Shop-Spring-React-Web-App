dd65ecc916d64ea7ebd028b08c377cf64a953b77
Avdo Sator <avdo.sator@hotmail.com>
2024-07-25 09:57:08 +0200

    Add method that finds all cartItems for given cart

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/repository/crud/CrudCartItemRepository.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/repository/crud/CrudCartItemRepository.java
@@ -4,8 +4,11 @@ import com.avdo.spring.app.repository.entity.CartItemEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 public interface CrudCartItemRepository extends JpaRepository<CartItemEntity, Long> {
 
     @Transactional
     CartItemEntity save(CartItemEntity cartItemEntity);
+    List<CartItemEntity> findByCartEntity_Id(Long cartId);
 }
