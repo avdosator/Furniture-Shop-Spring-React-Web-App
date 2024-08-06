0fd3ae3023f7667a5a0ad97ee935f4868f8b290c
Avdo Sator <avdo.sator@hotmail.com>
2024-08-06 20:28:03 +0200

    Add divs that are going to be container for main content

--- frontend/react-project/src/layouts/HomePage/ExploreTopProducts.tsx
+++ frontend/react-project/src/layouts/HomePage/ExploreTopProducts.tsx
@@ -1,5 +1,9 @@
 export default function ExploreTopProducts() {
     return (
-        
+        <div>
+            <div className="container-fluid py-5 text-white d-flex justify-content-center align-items-center">
+                
+            </div>
+        </div>
     )
 }
