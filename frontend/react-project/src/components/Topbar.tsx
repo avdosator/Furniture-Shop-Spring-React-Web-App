28113fd91c178e78eb29465014b38e722a761395
Avdo Sator <avdo.sator@hotmail.com>
2024-08-06 09:51:58 +0200

    Add Topbar buttons

--- frontend/react-project/src/components/Topbar.tsx
+++ frontend/react-project/src/components/Topbar.tsx
@@ -2,7 +2,9 @@ export default function Topbar() {
     return (
         <div className="bg-secondary text-white py-1">
             <div className="container-fluid d-flex justify-content-end mx-0">
-                
+                <button className="btn btn-sm btn-outline-light me-2">Login</button>
+                <button className="btn btn-sm btn-outline-light">Signup</button>
+                <button className="btn btn-sm btn-outline-light ms-2">Logout</button>
             </div>
         </div>
     )
