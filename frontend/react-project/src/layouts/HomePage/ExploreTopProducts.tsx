e8234a6a05b3edd69d7452176c5919bd53d1a3ec
Avdo Sator <avdo.sator@hotmail.com>
2024-08-06 20:34:24 +0200

    Add other elements of component

--- frontend/react-project/src/layouts/HomePage/ExploreTopProducts.tsx
+++ frontend/react-project/src/layouts/HomePage/ExploreTopProducts.tsx
@@ -4,6 +4,8 @@ export default function ExploreTopProducts() {
             <div className="container-fluid py-5 text-white d-flex justify-content-center align-items-center">
                 <div>
                 <h1 className="display-5 fw-bold">Find best products for you</h1>
+                <p className="col-md-8 fs-4">Explore our shop</p>
+                <a type="button" href="#" className="btn btn-lg main-color text-white">Explore Top Products</a>
                 </div>
             </div>
         </div>
