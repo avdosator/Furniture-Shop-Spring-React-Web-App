d24099703f08912bda6018529f53c64c1a081644
Avdo Sator <avdo.sator@hotmail.com>
2024-08-06 09:51:37 +0200

    Add another div of class d-flex that will contain buttons

--- frontend/react-project/src/components/Topbar.tsx
+++ frontend/react-project/src/components/Topbar.tsx
@@ -1,7 +1,9 @@
 export default function Topbar() {
     return (
         <div className="bg-secondary text-white py-1">
-            
+            <div className="container-fluid d-flex justify-content-end mx-0">
+                
+            </div>
         </div>
     )
 }
