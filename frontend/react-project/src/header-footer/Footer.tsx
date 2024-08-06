8eb21e7cf00793067a9bd0c2cb3c66079aedd65d
Avdo Sator <avdo.sator@hotmail.com>
2024-08-06 16:22:38 +0200

    Add placeholder for logo

--- frontend/react-project/src/header-footer/Footer.tsx
+++ frontend/react-project/src/header-footer/Footer.tsx
@@ -3,6 +3,7 @@ export default function Footer() {
         <div className="bg-dark text-secondary py-1">
             <footer className="container d-flex justify-content-around py-2">
             <p className="">&copy; Exclusive Furniture Store App</p>
+            <p>Placeholder for logo</p>
             <a className="" href="#">Contact</a>
             </footer>
         </div>
