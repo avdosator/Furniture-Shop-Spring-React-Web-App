e0c301834affaf0d5cd244d9b3d1fcbced2f6cfc
Avdo Sator <avdo.sator@hotmail.com>
2024-08-06 16:22:05 +0200

    Change positioning of elements in footer

--- frontend/react-project/src/header-footer/Footer.tsx
+++ frontend/react-project/src/header-footer/Footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
     return (
-        <div className="bg-dark text-secondary">
-            <footer>
-            <p>&copy; Exclusive Furniture Store App</p>
-            <a href="#">Contact</a>
+        <div className="bg-dark text-secondary py-1">
+            <footer className="container d-flex justify-content-around py-2">
+            <p className="">&copy; Exclusive Furniture Store App</p>
+            <a className="" href="#">Contact</a>
             </footer>
         </div>
     )
