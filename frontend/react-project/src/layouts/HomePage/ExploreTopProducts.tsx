9044cb0042fee92dcc8ad5a0fddb0c145a87604a
Avdo Sator <avdo.sator@hotmail.com>
2024-08-06 20:31:28 +0200

    Add heading of component

--- frontend/react-project/src/layouts/HomePage/ExploreTopProducts.tsx
+++ frontend/react-project/src/layouts/HomePage/ExploreTopProducts.tsx
@@ -1,8 +1,10 @@
 export default function ExploreTopProducts() {
     return (
-        <div>
+        <div className="header p-5 bg-dark mb-4">
             <div className="container-fluid py-5 text-white d-flex justify-content-center align-items-center">
-                
+                <div>
+                <h1 className="display-5 fw-bold">Find best products for you</h1>
+                </div>
             </div>
         </div>
     )
