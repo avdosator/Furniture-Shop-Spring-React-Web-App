b040a5e3a0d41cd9761f9b2147d38257479e21b4
Avdo Sator <avdo.sator@hotmail.com>
2024-08-07 16:45:19 +0200

    Add form that accepts users message

--- frontend/react-project/src/layouts/ContactPage/Contact.tsx
+++ frontend/react-project/src/layouts/ContactPage/Contact.tsx
@@ -12,6 +12,21 @@ export default function Contact() {
                 </div>
                 <div>
                     <h3>Send us a message</h3>
+                    <form>
+                        <div className="mb-3">
+                            <label htmlFor="name" className="form-label">Name</label>
+                            <input type="text" className="form-control" id="name" placeholder="Your Name" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="email" className="form-label">Email</label>
+                            <input type="email" className="form-control" id="email" placeholder="Your Email" />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="message" className="form-label">Message</label>
+                            <textarea className="form-control" id="message" rows={5} placeholder="Your Message"></textarea>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Send</button>
+                    </form>
                 </div>
             </div>
         </div>
