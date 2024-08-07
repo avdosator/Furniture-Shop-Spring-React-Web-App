f7e4751c26adfc7f361eaeaac63eafc8434dc576
Avdo Sator <avdo.sator@hotmail.com>
2024-08-07 20:55:47 +0200

    Add skeleton for page components

--- frontend/react-project/src/layouts/AboutPage/AboutPage.tsx
+++ frontend/react-project/src/layouts/AboutPage/AboutPage.tsx
@@ -2,6 +2,21 @@ import "./AboutPage.css"
 
 export default function AboutPage() {
     return (
-        
+        <div>
+            <div>
+                <div>
+                    <h1></h1>
+                    <p>
+
+                    </p>
+                </div>
+            </div>
+            <div>
+                <h1></h1>
+                <p>
+                    
+                </p>
+            </div>
+        </div>
     )
 }
