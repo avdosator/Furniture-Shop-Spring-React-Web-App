6ba95ade6df128f93d45ff9614380ef98b9f5cdf
Avdo Sator <avdo.sator@hotmail.com>
2024-08-07 16:42:41 +0200

    Add part where user can send a message

--- frontend/react-project/src/layouts/ContactPage/Contact.tsx
+++ frontend/react-project/src/layouts/ContactPage/Contact.tsx
@@ -10,6 +10,9 @@ export default function Contact() {
                     <p><strong>Email: </strong> avdo.sator@hotmail.com</p>
                     <p><strong>LinkedIn: </strong><a href="https://www.linkedin.com/in/avdo-%C5%A1ator-725370308/">Avdo Sator</a></p>
                 </div>
+                <div>
+                    <h3>Send us a message</h3>
+                </div>
             </div>
         </div>
     )
