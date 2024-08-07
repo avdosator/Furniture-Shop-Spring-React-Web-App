422faa15250ad574cfa98a51d8e098b4eeba4243
Avdo Sator <avdo.sator@hotmail.com>
2024-08-07 11:04:35 +0200

    Add some style to footer to make it thicker and move it to the bottom of screen

--- frontend/react-project/src/header-footer/Footer.tsx
+++ frontend/react-project/src/header-footer/Footer.tsx
@@ -1,6 +1,8 @@
+import "./Footer.css"
+
 export default function Footer() {
     return (
-        <div className="bg-dark text-secondary py-1">
+        <div className="bg-dark text-secondary py-1 footer">
             <footer className="container d-flex justify-content-around py-2">
             <p className="">&copy; Exclusive Furniture Store App</p>
             <p>Placeholder for logo</p>
