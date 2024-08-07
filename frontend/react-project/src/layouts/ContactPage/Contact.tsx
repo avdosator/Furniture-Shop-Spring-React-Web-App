5ef2ff45c29b04ec0b934afe140c3ddf99f45d70
Avdo Sator <avdo.sator@hotmail.com>
2024-08-07 16:35:39 +0200

    Add main contact informations

--- frontend/react-project/src/layouts/ContactPage/Contact.tsx
+++ frontend/react-project/src/layouts/ContactPage/Contact.tsx
@@ -1,5 +1,16 @@
 export default function Contact() {
     return (
-        
+        <div>
+            <h1></h1>
+            <div className="row">
+                <div>
+                    <h3>Contact information</h3>
+                    <p><strong>Address: </strong>Grand opening of store in Sarajevo - SOON!</p>
+                    <p><strong>Phone: </strong> +38700-000-000</p>
+                    <p><strong>Email: </strong> avdo.sator@hotmail.com</p>
+                    <p><strong>LinkedIn: </strong><a href="https://www.linkedin.com/in/avdo-%C5%A1ator-725370308/">Avdo Sator</a></p>
+                </div>
+            </div>
+        </div>
     )
 }
