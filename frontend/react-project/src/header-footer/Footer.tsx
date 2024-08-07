407e013234c417115169077489f3f53ec5cde140
Avdo Sator <avdo.sator@hotmail.com>
2024-08-07 17:40:38 +0200

    Make footer same color as navbar

--- frontend/react-project/src/header-footer/Footer.tsx
+++ frontend/react-project/src/header-footer/Footer.tsx
@@ -2,7 +2,7 @@ import "./Footer.css"
 
 export default function Footer() {
     return (
-        <div className="bg-dark text-secondary py-1 footer">
+        <div className="bg-dark text-secondary py-1 footer bg-body-tertiary" data-bs-theme="dark">
             <footer className="container d-flex justify-content-around py-2">
             <p className="">&copy; Exclusive Furniture Store App</p>
             <p>Placeholder for logo</p>
