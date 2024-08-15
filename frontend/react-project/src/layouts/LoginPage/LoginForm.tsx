9ec268ccf1c6dc4cc76535790332756823b108b0
Avdo Sator <avdo.sator@hotmail.com>
2024-08-15 19:26:43 +0200

    Implement handleChange function

--- frontend/react-project/src/layouts/LoginPage/LoginForm.tsx
+++ frontend/react-project/src/layouts/LoginPage/LoginForm.tsx
@@ -3,8 +3,19 @@ import { useState } from "react";
 export default function LoginForm() {
     let [formData, setFormData] = useState({username: "", password: ""});
 
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>): void {
+        let {name, value} = e.target as HTMLInputElement;
+        setFormData(oldData => {
+            return {
+                ...oldData,
+                [name]: value
+            }
+        })
+    }
+
     function handleSubmit(e:React.FormEvent<HTMLFormElement> ): void {
         e.preventDefault();
+        console.log(formData);
         //logic for sending data to server
     }
 
@@ -16,6 +27,7 @@ export default function LoginForm() {
                 value={formData.username}
                 id="username"
                 name="username"
+                onChange={handleChange}
                 placeholder="Enter your username here"
                 autoFocus
             />
@@ -24,6 +36,7 @@ export default function LoginForm() {
                 value={formData.password}
                 id="password"
                 name="password"
+                onChange={handleChange}
                 placeholder="Password"
             />
             <button type="submit">Log In</button>
