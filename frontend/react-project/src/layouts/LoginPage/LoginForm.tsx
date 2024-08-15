96453151e120ec55cdc3f7f2c776bda7a63a8f54
Avdo Sator <avdo.sator@hotmail.com>
2024-08-15 18:54:05 +0200

    Add main form elements

--- frontend/react-project/src/layouts/LoginPage/LoginForm.tsx
+++ frontend/react-project/src/layouts/LoginPage/LoginForm.tsx
@@ -1,5 +1,15 @@
 export default function LoginForm() {
     return (
-        
+        <form >
+            <label htmlFor="username">Username</label>
+            <input type="text"
+                
+            />
+            <label htmlFor="password">Password</label>
+            <input type="password"
+                
+            />
+            
+        </form>
     )
 }
