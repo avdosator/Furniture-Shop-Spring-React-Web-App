9f4749d25040f95982d826bf41a7c114341504b7
Avdo Sator <avdo.sator@hotmail.com>
2024-08-17 11:54:47 +0200

    Add name attribute to all fields

--- frontend/react-project/src/layouts/SignupPage/SignupForm.tsx
+++ frontend/react-project/src/layouts/SignupPage/SignupForm.tsx
@@ -6,30 +6,35 @@ export default function SignupForm() {
                 <label htmlFor="firstname">Firstname</label>
                 <input type="text"
                     id="firstname"
+                    name="firstname"
                 />
             </div>
             <div>
                 <label htmlFor="lastname">Lastname</label>
                 <input type="text"
                     id="lastname"
+                    name="firstname"
                 />
             </div>
             <div>
                 <label htmlFor="createUserUsername">Username</label>
                 <input type="text"
                     id="createUserUsername"
+                    name="firstname"
                 />
             </div>
             <div>
                 <label htmlFor="createUserPassword">Password</label>
                 <input type="password"
                     id="createUserPassword"
+                    name="firstname"
                 />
             </div>
             <div>
                 <label htmlFor="email">Email</label>
                 <input type="email"
                     id="email"
+                    name="firstname"
                 />
             </div>
         </form>
