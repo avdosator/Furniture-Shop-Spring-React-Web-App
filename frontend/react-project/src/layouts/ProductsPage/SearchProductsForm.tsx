e8cdb5ea0aae4472b232d8b5512dff48edc85929
Avdo Sator <avdo.sator@hotmail.com>
2024-08-17 20:58:53 +0200

    Add inputs and labels

--- frontend/react-project/src/layouts/ProductsPage/SearchProductsForm.tsx
+++ frontend/react-project/src/layouts/ProductsPage/SearchProductsForm.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export default function SearchProductsForm() {
     let [searchParameters, setSearchParameters] = useState({ productName: "", category: "", sortBy: "" });
-    let [priceRange, setPriceRange] = useState({minPrice: 0, maxPrice: 10000}); //implement to set maxPrice to most expensive product price
+    let [priceRange, setPriceRange] = useState({ minPrice: 0, maxPrice: 10000 }); //implement to set maxPrice to most expensive product price
 
     function handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
         e.preventDefault();
@@ -10,26 +10,30 @@ export default function SearchProductsForm() {
         console.log(priceRange);
         //logic for searching products
         setSearchParameters({ productName: "", category: "", sortBy: "" });
-        setPriceRange({minPrice: 0, maxPrice: 10000});
+        setPriceRange({ minPrice: 0, maxPrice: 10000 });
     }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <div>
-
+                    <label htmlFor=""></label>
+                    <input type="text" />
                 </div>
                 <div>
-
+                    <label htmlFor=""></label>
+                    <input type="text" />
                 </div>
                 <div>
-
+                    <label htmlFor=""></label>
+                    <input type="text" />
                 </div>
                 <div>
-
+                    <label htmlFor=""></label>
+                    <input type="text" />
                 </div>
-                <button></button>
-                <button></button>
+                <button type="submit">Submit</button>
+                <button type="reset">Reset</button>
             </form>
         </div>
     )
