3063c6e19ea1b1ee46cd3a73d585847ca823ddc7
Avdo Sator <avdo.sator@hotmail.com>
2024-08-17 20:33:58 +0200

    Create basic structure of component

--- frontend/react-project/src/layouts/ProductsPage/SearchProductsForm.tsx
+++ frontend/react-project/src/layouts/ProductsPage/SearchProductsForm.tsx
@@ -1,5 +1,22 @@
 export default function SearchProductsForm() {
     return (
-        
+        <div>
+            <form onSubmit={}>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+                <div>
+                    
+                </div>
+                <button></button>
+                <button></button>
+            </form>
+        </div>
     )
 }
