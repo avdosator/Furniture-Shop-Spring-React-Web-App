79265abab44e555b4c1f5fe12a9804ebe8e22de7
Avdo Sator <avdo.sator@hotmail.com>
2024-08-17 20:57:30 +0200

    Implement state

--- frontend/react-project/src/layouts/ProductsPage/SearchProductsForm.tsx
+++ frontend/react-project/src/layouts/ProductsPage/SearchProductsForm.tsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
+
 export default function SearchProductsForm() {
+    let [searchParameters, setSearchParameters] = useState({ productName: "", category: "", sortBy: "" });
+    let [priceRange, setPriceRange] = useState({minPrice: 0, maxPrice: 10000}); //implement to set maxPrice to most expensive product price
+
     function handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
         e.preventDefault();
         console.log(searchParameters);
+        console.log(priceRange);
         //logic for searching products
+        setSearchParameters({ productName: "", category: "", sortBy: "" });
+        setPriceRange({minPrice: 0, maxPrice: 10000});
     }
 
     return (
