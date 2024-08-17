546cb82b57433475dd7d48f350233a7d06a4af89
Avdo Sator <avdo.sator@hotmail.com>
2024-08-17 11:49:48 +0200

    Add fields for user creation

--- frontend/react-project/src/layouts/SignupPage/SignupForm.tsx
+++ frontend/react-project/src/layouts/SignupPage/SignupForm.tsx
@@ -2,6 +2,26 @@ export default function SignupForm() {
     return (
         <form>
             <h4>Sign up</h4>
+            <div>
+                <label htmlFor="">Firstname</label>
+                <input type="text" />
+            </div>
+            <div>
+                <label htmlFor="">Lastname</label>
+                <input type="text" />
+            </div>
+            <div>
+                <label htmlFor="">Username</label>
+                <input type="text" />
+            </div>
+            <div>
+                <label htmlFor="">Password</label>
+                <input type="password" />
+            </div>
+            <div>
+                <label htmlFor="">Email</label>
+                <input type="email" />
+            </div>
         </form>
     )
 }
