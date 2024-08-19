451b712013f348c95042407aebf3286c64a5a1f6
Avdo Sator <avdo.sator@hotmail.com>
2024-08-19 14:13:47 +0200

    Create ProductItem component

--- frontend/react-project/src/layouts/ProductsPage/ProductsList.tsx
+++ frontend/react-project/src/layouts/ProductsPage/ProductsList.tsx
@@ -1,6 +1,12 @@
+import Category from "../../models/Category";
+import Product from "../../models/Product";
 import SearchProductsForm from "./SearchProductsForm";
 
 export default function ProductsList() {
+
+    let products: Product[] = [];
+    products.push(new Product(1, "iron chair", 50.0, 1, "The best chair", new Category(1, "chair")));
+    products.push(new Product(2, "double bed", 995.0, 1, "Top quality", new Category(1, "bed")));
     return (
         <>
             <SearchProductsForm />
