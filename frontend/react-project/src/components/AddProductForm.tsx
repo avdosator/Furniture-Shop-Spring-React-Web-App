3501a47031114c8750e732af03d35ac706262686
Avdo Sator <avdo.sator@hotmail.com>
2024-08-19 10:32:00 +0200

    Add state model for this form

--- frontend/react-project/src/components/AddProductForm.tsx
+++ frontend/react-project/src/components/AddProductForm.tsx
@@ -1,5 +1,13 @@
-export default function AddProductForm() {
-    return (
+import { useState } from "react";
 
+export default function AddProductForm() {
+    let [formData, setFormData] = useState({ name: "", price: 0, stock: 0, description: "", category: "" });
+
+    
+
+    return (
+        <div>
+
+        </div>
     );
 }
