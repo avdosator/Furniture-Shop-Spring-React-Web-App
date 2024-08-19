7cf8309e3c6afe98ad23bfc8e1e3a386d1567411
Avdo Sator <avdo.sator@hotmail.com>
2024-08-19 10:25:22 +0200

    Create AddProductForm component

--- frontend/react-project/src/routes/ProductRoutes.tsx
+++ frontend/react-project/src/routes/ProductRoutes.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import SearchProductsForm from "../layouts/ProductsPage/SearchProductsForm";
+import AddProductForm from "../components/AddProductForm";
 
 export default function ProductRoutes() {
     return (
         <Routes>
             <Route index element={<SearchProductsForm /> /* this element will be part of ProductList component */} />
             <Route path=':id' element={<h1>Placeholder for single product page</h1>} />
-            <Route path='new' element={<h1>Placeholder for adding new product</h1>} />
+            <Route path='new' element={<AddProductForm />} />
         </Routes>
     );
 }
