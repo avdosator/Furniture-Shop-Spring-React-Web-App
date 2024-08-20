63474ff1b54fd2415734f84a1d60eac348e8483c
Avdo Sator <avdo.sator@hotmail.com>
2024-08-20 17:44:26 +0200

    Make borders of card and card image more rounded

--- frontend/react-project/src/layouts/ProductsPage/ProductItem.tsx
+++ frontend/react-project/src/layouts/ProductsPage/ProductItem.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 import Category from "../../models/Category"
+import "./ProductItem.css"
 
 type ProductItemProps = {
     id: number,
@@ -13,8 +14,8 @@ type ProductItemProps = {
 export default function ProductItem({ id, name, price, stock, description, category }: ProductItemProps) {
     return (
         <Link to={`/products/:{id}`} style={{textDecoration:"none"}}>
-        <div className="card shadow">
-            <img src="https://via.placeholder.com/150x100" className="card-img-top img-fluid rounded p-2" alt="..." />
+        <div className="card shadow product-item">
+            <img src="https://via.placeholder.com/150x100" className="card-img-top p-3 card-image" alt="..." />
             <div className="card-body">
                 <h5 className="card-title text-center">{name}</h5>
                 <h6 className="card-text text-center">${price.toFixed(2)}</h6>
