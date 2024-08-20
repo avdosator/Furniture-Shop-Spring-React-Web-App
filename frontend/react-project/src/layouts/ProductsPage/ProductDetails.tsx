47a70cd90098bad3633e0dc5b1fddea095946922
Avdo Sator <avdo.sator@hotmail.com>
2024-08-20 21:53:26 +0200

    Send data to ProductDetails with state attribute

--- frontend/react-project/src/layouts/ProductsPage/ProductDetails.tsx
+++ frontend/react-project/src/layouts/ProductsPage/ProductDetails.tsx
@@ -1,7 +1,12 @@
+import { useLocation } from "react-router-dom"
+
 export default function ProductDetails() {
+    const location = useLocation();
+    const {id, name, price, stock, description, category} = location.state || {};
     return (
         <div>
-            
+            <h1>{name}</h1>
+            <h2>{price}</h2>
         </div>
     )
 }
--- frontend/react-project/src/routes/ProductRoutes.tsx
+++ frontend/react-project/src/routes/ProductRoutes.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import AddProductForm from "../components/AddProductForm";
 import ProductsList from "../layouts/ProductsPage/ProductsList";
+import ProductDetails from "../layouts/ProductsPage/ProductDetails";
 
 export default function ProductRoutes() {
     return (
         <Routes>
             <Route index element={<ProductsList />} />
-            <Route path=':id' element={<h1>Placeholder for single product page</h1>} />
+            <Route path=':id' element={<ProductDetails />} />
             <Route path='new' element={<AddProductForm />} />
         </Routes>
     );
