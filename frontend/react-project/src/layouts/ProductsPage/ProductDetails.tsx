c6da8521b2957d6786715c16528f9bc492f4c300
Avdo Sator <avdo.sator@hotmail.com>
2024-08-20 22:14:32 +0200

    Create structure for ProductDetails component

--- frontend/react-project/src/layouts/ProductsPage/ProductDetails.tsx
+++ frontend/react-project/src/layouts/ProductsPage/ProductDetails.tsx
@@ -5,8 +5,16 @@ export default function ProductDetails() {
     const {id, name, price, stock, description, category} = location.state || {};
     return (
         <div>
-            <h1>{name}</h1>
-            <h2>{price}</h2>
+            <div className="col-6">
+                <img src="https://via.placeholder.com/300x200" alt="" />
+            </div>
+            <div className="col-6">
+                <h1 className="">{name}</h1>
+                <h3 className="text-capitalize">Category: {category.name}</h3>
+                <p>{price}</p>
+                <p>{description}</p>
+                <button>Add to cart</button>
+            </div>
         </div>
     )
 }
