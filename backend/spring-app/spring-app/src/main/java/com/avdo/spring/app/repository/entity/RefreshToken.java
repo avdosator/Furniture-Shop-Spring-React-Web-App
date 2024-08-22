2363e07f92e763ca67bcd8f1b37ab3c0e0fa2fbc
Avdo Sator <avdo.sator@hotmail.com>
2024-08-22 14:48:09 +0200

    Add fields to RefreshToken entit

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/repository/entity/RefreshToken.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/repository/entity/RefreshToken.java
@@ -2,11 +2,30 @@ package com.avdo.spring.app.repository.entity;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.Date;
 
 @Entity
 @Table(name="refresh_token")
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class RefreshToken {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "token_hash", nullable = false, length = 128, unique = true)
+    private String tokenHash;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @Column(name = "expiry_date", nullable = false)
+    private Date expiryDate;
+
+    @Column(name = "created_at", nullable = false)
+    private Date createdAt;
 }
