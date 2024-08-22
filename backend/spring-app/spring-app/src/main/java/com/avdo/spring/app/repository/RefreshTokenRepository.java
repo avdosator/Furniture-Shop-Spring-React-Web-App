71721d6ddcd58e4a6fbc9c4c079c9961d4535efa
Avdo Sator <avdo.sator@hotmail.com>
2024-08-22 16:47:42 +0200

    Create RefreshTokenRepository interface

---
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.avdo.spring.app.repository;
+
+import com.avdo.spring.app.service.domain.model.RefreshToken;
+import com.avdo.spring.app.service.domain.model.User;
+
+public interface RefreshTokenRepository {
+
+    String createRefreshToken(User user);
+    RefreshToken validateToken(String token);
+    void deleteToken(String token);
+}
