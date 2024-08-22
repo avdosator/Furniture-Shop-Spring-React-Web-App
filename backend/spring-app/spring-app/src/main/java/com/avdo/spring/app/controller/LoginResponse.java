a1219ca0d229e2eecf5d81b4c3c2c6dfcd817e96
Avdo Sator <avdo.sator@hotmail.com>
2024-08-22 17:57:09 +0200

    Add refreshToken field in LoginResponse

--- backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/controller/LoginResponse.java
+++ backend/spring-app/spring-app/src/main/java/com/avdo/spring/app/controller/LoginResponse.java
@@ -2,22 +2,31 @@ package com.avdo.spring.app.controller;
 
 public class LoginResponse {
 
-    private String token;
+    private String accessToken;
     private long expiresIn;
+    private String refreshToken;
 
-    public String getToken() {
-        return this.token;
+    public String getAccessToken() {
+        return this.accessToken;
     }
 
     public long getExpiresIn() {
         return this.expiresIn;
     }
 
-    public void setToken(String token) {
-        this.token = token;
+    public String getRefreshToken() {
+        return this.refreshToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
     }
 
     public void setExpiresIn(long expiresIn) {
         this.expiresIn = expiresIn;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
     }
 }
