501bef982528a312d23b970b3b21be32a36db2f2
Avdo Sator <avdo.sator@hotmail.com>
2024-08-27 10:15:22 +0200

    Implement call method that can handle various api calls

--- frontend/react-project/src/service/ApiService.ts
+++ frontend/react-project/src/service/ApiService.ts
@@ -1,3 +1,23 @@
 export default class ApiService {
-    
+
+    static async call<T>(route: string, method: string = "GET", body: any = null, headers: Record<string, string> = {}): Promise<T> {
+        const options: RequestInit = {
+            method: method,
+            headers: {
+                "Content-Type": "application/json",
+                ...headers
+            }
+        }
+
+        if (body) options.body = JSON.stringify(body);
+
+        try {
+            const response = await fetch(`http://localhost:8080/${route}`, options);
+            if (!response.ok) throw new Error(response.statusText);
+            return await response.json();
+        } catch (error) {
+            console.error("Api call failed ", error);
+            throw error;
+        }
+    }
 }
