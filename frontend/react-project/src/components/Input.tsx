da1580ad82ccad3bc6cb17996891f8d289648d16
Avdo Sator <avdo.sator@hotmail.com>
2024-08-28 12:18:51 +0200

    Create structure of Input component with all possible props

--- frontend/react-project/src/components/Input.tsx
+++ frontend/react-project/src/components/Input.tsx
@@ -1,7 +1,14 @@
-export default function Input() {
+export default function Input({type, classname, value, id, name, onChange, placeholder, autoComplete, autoFocus }) {
     return (
-        <>
-        
-        </>
+        <input type="text"
+        className=""
+        value={}
+        id=""
+        name=""
+        onChange={}
+        placeholder=""
+        autoComplete=""
+        autoFocus
+         />
     )
 }
