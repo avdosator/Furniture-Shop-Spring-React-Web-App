72dd30d3dc43c28b344bbc779b18831e94d7161c
Avdo Sator <avdo.sator@hotmail.com>
2024-08-28 12:29:45 +0200

    Create InputProps type

--- frontend/react-project/src/components/Input.tsx
+++ frontend/react-project/src/components/Input.tsx
@@ -1,4 +1,16 @@
-export default function Input({type, classname, value, id, name, onChange, placeholder, autoComplete, autoFocus }) {
+type InputProps = {
+    type: string,
+    className: string,
+    value: string | number,
+    id: string,
+    name: string,
+    onChange: Function,
+    placeholder: string,
+    autoComplete: string,
+    autofocus: boolean 
+}
+
+export default function Input({type, classname, value, id, name, onChange, placeholder, autoComplete, autoFocus }: InputProps) {
     return (
         <input type="text"
         className=""
