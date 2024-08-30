a607c91f5dcbd07c8d4f5324aa2938f1a0cef76e
Avdo Sator <avdo.sator@hotmail.com>
2024-08-30 09:35:08 +0200

    Create CategoryContext and CategoryContextProps type

--- frontend/react-project/src/context/CategoryContext.tsx
+++ frontend/react-project/src/context/CategoryContext.tsx
@@ -1,5 +1,16 @@
-export default function CategoryProvider() {
+import { createContext, ReactNode } from "react"
+
+type CategoryContextProps = {
+    categories: {
+        id: number,
+        name: string
+    }
+}
+
+const CategoryContext = createContext<CategoryContextProps | undefined>(undefined);
+
+export default function CategoryProvider({children}: {children: ReactNode}) {
     return (
-        
+
     )
 }
