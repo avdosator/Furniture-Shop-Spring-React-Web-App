3cae675398816ffb70b06c27a5789dcb4a586abf
Avdo Sator <avdo.sator@hotmail.com>
2024-09-03 12:49:39 +0200

    Finish CategoryContext component and create custom hook that returns fetched categories

--- frontend/react-project/src/context/CategoryContext.tsx
+++ frontend/react-project/src/context/CategoryContext.tsx
@@ -1,16 +1,37 @@
-import { createContext, ReactNode } from "react"
+import { createContext, ReactNode, useContext, useEffect, useState } from "react"
+import ApiService from "../service/ApiService";
+import Category from "../models/Category";
 
 type CategoryContextProps = {
-    categories: {
-        id: number,
-        name: string
-    }
+    categories: Category[]
 }
 
 const CategoryContext = createContext<CategoryContextProps | undefined>(undefined);
 
-export default function CategoryProvider({children}: {children: ReactNode}) {
-    return (
+export default function CategoryProvider({ children }: { children: ReactNode }) {
+    let [categories, setCategories] = useState<Category[]>([]);
 
+    useEffect(() => {
+        const fetchCategories = async () => {
+            let categories = await ApiService.call<Category[]>("api/categories", "GET");
+            setCategories(categories.map((category) => {
+                return new Category(category.getId(), category.getName());
+            }));
+        }
+        fetchCategories();
+    }, []);
+
+    return (
+        <CategoryContext.Provider value={{ categories }} >
+            {children}
+        </CategoryContext.Provider >
     )
+}
+
+export function useCategories(): CategoryContextProps {
+    const context = useContext(CategoryContext);
+    if (!context) {
+        throw new Error("useCategories must be used within a CategoryProvider");
+    }
+    return context;
 }
