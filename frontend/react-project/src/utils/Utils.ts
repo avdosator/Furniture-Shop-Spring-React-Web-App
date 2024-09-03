12c48632df2b72939ba162943ebf5a71e26e7bd7
Avdo Sator <avdo.sator@hotmail.com>
2024-09-03 17:10:30 +0200

    Remove unused getCategories method

--- frontend/react-project/src/utils/Utils.ts
+++ frontend/react-project/src/utils/Utils.ts
@@ -1,8 +1,4 @@
 export default class Utils {
-    static getCategories(): string[] {
-        return ["all", "chair", "double bed", "kitchen", "table"]; 
-    }
-
     static getSortingOptions(): string[] {
        return ["name (ascending)", "name (descending)", "price (ascending)", "price (descending)", "newest"];
     }
